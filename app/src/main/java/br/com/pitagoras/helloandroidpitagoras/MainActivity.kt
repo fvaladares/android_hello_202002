63159caf86bbb224be152c6df7dd760019c5581a
Fabricio Valadares <fabricio.valadares@gmail.com>
2020-09-03 21:25:33 -0300

    Insert a ScrowView

--- app/src/main/java/br/com/pitagoras/helloandroidpitagoras/MainActivity.kt
+++ app/src/main/java/br/com/pitagoras/helloandroidpitagoras/MainActivity.kt
@@ -1,14 +1,23 @@
 package br.com.pitagoras.helloandroidpitagoras
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "<<<FGV>>>"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_send.setOnClickListener {
+            lerDados()
+        }
 //        val tv_first = findViewById<TextView>(R.id.tv_first_text)
 //        tv_first.text = getString(R.string.um_texto_qq)
+    }
+
+    fun lerDados() {
+        val course_name = et_course.text.toString()
+        tv_answare.text = tv_answare.text.toString() + ": " + course_name
     }
 }
